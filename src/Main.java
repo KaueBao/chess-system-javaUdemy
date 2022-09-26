d5589286ca0ef4320df7bec5ee8447980fccc14f
KaueBao <kaueth123_@hotmail.com>
2022-09-26 11:37:16 -0300

    Piece and Board

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
+import boardgame.Board;
 import boardgame.Position;
 
 public class Main {
     public static void main(String[] args) {
 
-
-        Position ps = new Position(1,4);
-
-        System.out.println(ps.toString());
+        Board board = new Board(8,8);
     }
 }
