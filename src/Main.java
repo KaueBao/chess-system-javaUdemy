c49fdc8298351fd7e302876535d357dc2e828f87
KaueBao <kaueth123_@hotmail.com>
2022-09-26 11:24:33 -0300

    teste

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import boardgame.Position;
+
 public class Main {
     public static void main(String[] args) {
 
+
+        Position ps = new Position(1,4);
+
+        System.out.println(ps.toString());
     }
 }
